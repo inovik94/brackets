fb8f7e43bbf9f5b7e6beb62b24fc07a19cdd4c7f
“inovik94” <ilya.novikk@gmail.com>
2021-03-13 18:58:38 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+"use strict";
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) return false;
+  var newstr = str;
+  var newarr = bracketsConfig.map(function (miniarr) {
+    return miniarr.join("");
+  });
+
+  while (newstr.length > 0) {
+    for (var i = 0; i < newarr.length; i++) {
+      if (newstr.includes(newarr[i])) {
+        newstr = newstr.replace(newarr[i], "");
+      }
+    }
+
+    if (newarr.every(function (brackets) {
+      return newstr.includes(brackets) === false;
+    })) break;
+  }
+
+  return newstr.length === 0;
+};
